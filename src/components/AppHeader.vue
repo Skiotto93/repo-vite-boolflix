ab75406611505973589842f813387bdf92538495
skiotto93 <fabio.forenza@hotmail.com>
2022-11-30 12:29:55 +0100

    Aggiunto component AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+<header>
+    <div class="container">
+        <h1>BOOLFLIX</h1>
+        <form>
+            <input type="text" placeholder="Inserisci testo">
+        </form>
+    </div>
+</header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    background-color: #000;
+    padding: 1.25rem;
+    
+    .container {
+    margin:  auto;
+    max-width: 75rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    h1 {
+        color: red;
+    }
+    }
+}
+
+</style>
